3edbbbd9dffee4318ae9defe3876c4a1a3dd6334
TatsianaSlapik <slapik.tatiana@gmail.com>
2020-10-01 22:57:07 +0300

    feat: added functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array || array.length == 0) {
+        return 0;
+    } else {
+        let min = Math.min(...array);
+        return min;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || array.length == 0) {
+        return 0;
+    } else {
+        let max = Math.max(...array);
+        return max;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+
+    if (!array || array.length == 0) {
+        return 0;
+    } else {
+        var sum = 0;
+        for (var i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        let avg = sum / array.length;
+
+        return avg;
+    }
 }
